cd2fd20c4bdcfedf12f764c0df8ee2e9735ba7e5
armentroutdd <armentroutdd@cota.com>
2022-02-24 16:28:47 -0500

    improved search results, added endpoints

--- server.js
+++ server.js
@@ -38,6 +38,31 @@ app.post('/add', (req, res) => {
   });
 });
 
+app.delete('/delete/:id', (req, res) => {
+  // curl -X DELETE localhost:3000/delete/12
+  const query = 'DELETE FROM records WHERE id = ?';
+  db.run(query, [req.params.id], function(err){ 
+    if(err){
+      res.end(err.message);
+      return;
+    }
+    res.end(`record ID #${req.params.id} deleted`);
+  });
+});
+
+app.put('/update/:id', (req, res) => {
+  // curl -X PUT -d "artist=the%20cure&title=wish&year=1994&media=cd&notes=" localhost:3000/update/1
+  const query = `UPDATE records SET artist = ?, title = ?, year = ?, media = ?, notes = ? WHERE id = ${req.params.id}`;
+  const values = Object.values(req.body);
+  db.run(query, values, function(err){ // classic function to be able to use this
+    if(err){
+      res.end(err.message);
+      return;
+    }
+    res.end(`record ID #${req.params.id} updated`);
+  });
+});
+
 app.get('/id/:id', (req, res) => {
   const query = 'SELECT * FROM records WHERE ID = ?';
   db.all(query, [req.params.id], function(err, rows){
@@ -45,19 +70,50 @@ app.get('/id/:id', (req, res) => {
       res.end(err.message);
       return;
     }
+    if(rows.length <= 0){
+      res.json({ "results": "none"});
+      return;
+    }
     res.json(rows[0]);
   });
 });
 
-app.get('/artists', (req, res) => {
+app.get('/artist-list', (req, res) => {
   const query = 'SELECT artist FROM records ORDER BY artist ASC';
   db.all(query, [], function(err, rows){
     if(err){
       res.end(err.message);
       return;
     }
+    if(rows.length <= 0){
+      res.json({ "results": "none"});
+      return;
+    }
     const artistArray = rows.map(x => x.artist);
-    res.json(artistArray);
+    const dedupedArtistArray = [...new Set(artistArray)];
+    res.json(dedupedArtistArray);
+  });
+});
+
+app.get('/artist/:artist', (req, res) => {
+  let query = "SELECT * FROM records WHERE artist = ? ORDER BY ";
+  const filters = ['title', 'year', 'media', 'notes'];
+  if(req.query.filter && filters.indexOf(req.query.filter) > -1){  
+    query += `${req.query.filter} ASC`;
+  }
+  else {
+    query += 'title ASC';
+  }
+  db.all(query, [req.params.artist], function(err, rows){
+    if(err){
+      res.end(err.message);
+      return;
+    }
+    if(rows.length <= 0){
+      res.json({ "results": "none"});
+      return;
+    }
+    res.json(rows);
   });
 });
 
@@ -68,18 +124,35 @@ app.get('/random', (req, res) => {
       res.end(err.message);
       return;
     }
+    if(rows.length <= 0){
+      res.json({ "results": "none"});
+      return;
+    }
     res.json(rows[0]);
   });
 });
 
-// search by different properties (artist, year, title)
-
 app.get('/search/:needle', (req, res) => { 
   // concatenate (||) the wildcards (%) with the placeholder (?)
-  const query = "SELECT * FROM records WHERE artist LIKE '%' || ? || '%' OR title LIKE '%' || ? || '%'";
-  db.all(query, [req.params.needle, req.params.needle], function(err, rows){
+  let query = "SELECT * FROM records WHERE \
+         artist LIKE '%' || $needle || '%' \
+       OR title LIKE '%' || $needle || '%' \
+       OR notes LIKE '%' || $needle || '%' ORDER BY ";
+  const filters = ['artist', 'title', 'year', 'media', 'notes'];
+  if(req.query.filter && filters.indexOf(req.query.filter) > -1){  
+    query += `${req.query.filter} ASC`;
+  }
+  else {
+    query += 'artist ASC';
+
+  }
+  db.all(query, { $needle: req.params.needle}, function(err, rows){
     if(err){
       res.end(err.message);
+      return;
+    }
+    if(rows.length <= 0){
+      res.json({ "results": "none"});
       return;
     }
     res.json(rows);
